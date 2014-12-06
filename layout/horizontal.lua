6dad4f617427bad1f6fb0a13ed3a8deb196d33e2
andy <andy@andydote.co.uk>
2014-12-06 19:35:43 +0000

    layout: add horizontal layout strategy and tests

---
+++ layout/horizontal.lua
@@ -0,0 +1,60 @@
+if require then
+	require("framemodel")
+	require("horizontaltests")
+else
+	local addon, ns = ...
+end
+
+local horizontal = function(settings, container, children)
+
+
+	local direction = 1
+
+	if settings.origin:find("RIGHT") then
+		direction = -1
+	end
+
+
+	local x = 0
+	local y = 0
+
+	local rowHeight = 0
+
+	for i, child in ipairs(children) do
+
+		if settings.wrap and x + child:GetWidth() > container:GetWidth() then
+			x = 0
+			y = y + rowHeight + settings.itemSpacing
+			rowHeight = 0
+		end
+
+		child:SetPoint(settings.origin, container, settings.origin, x * direction, -y)
+
+		x = x + child:GetWidth() + settings.itemSpacing
+
+		if child:GetHeight() > rowHeight then
+			rowHeight = child:GetHeight()
+		end
+
+	end
+
+	if settings.autosize == "x" or settings.autosize == "both" then
+
+		x = x - settings.itemSpacing
+		container:SetWidth(x)
+
+	end
+
+	if settings.autosize == "y" or settings.autosize == "both" then
+		y = y + rowHeight
+		container:SetHeight(y)
+	end
+
+end
+
+if RunTests then
+	RunTests({ horizontal = horizontal })
+else
+	ns.layoutEngine:addStrategy("horizontal", horizontal)
+end
+
---
+++ layout/horizontaltests.lua
@@ -0,0 +1,85 @@
+local tests = {
+
+	autosize_two_frames_no_wrap = function(engine)
+
+		local settings = {
+			origin = "TOPLEFT",
+			wrap = false,
+			itemSpacing = 5,
+			autosize = "both",
+		}
+
+		local container = Frame:new(500, 500)
+		local frames = { Frame:new(10, 10), Frame:new(10, 10)}
+
+		engine.horizontal(settings, container, frames)
+
+		assert(container.width == 25)
+		assert(container.height == 10)
+
+	end,
+
+	autosize_two_frames_wrap = function(engine)
+
+		local settings = {
+			origin = "TOPLEFT",
+			wrap = true,
+			itemSpacing = 5,
+			autosize = "both",
+		}
+
+		local container = Frame:new(15, 500)
+		local frames = { Frame:new(10, 10), Frame:new(10, 10)}
+
+		engine.horizontal(settings, container, frames)
+
+		assert(container.width == 10)
+		assert(container.height == 25)
+
+	end,
+
+	auto_size_three_frames_wrap = function(engine)
+
+		local settings = {
+			origin = "TOPLEFT",
+			wrap = true,
+			itemSpacing = 5,
+			autosize = "both",
+		}
+
+		local container = Frame:new(30, 500)
+		local frames = { Frame:new(10, 10), Frame:new(10, 10), Frame:new(55, 10)}
+
+		engine.horizontal(settings, container, frames)
+
+		assert(container.width == 55)
+		assert(container.height == 25)
+
+	end,
+}
+
+RunTests = function(engine)
+
+	for name, test in pairs(tests) do
+
+		local status, message = pcall(function() test(engine) end)
+
+		if status then
+			status = "passed"
+		else
+			status = message
+		end
+
+		print(string.format("Test %s: %s", name:gsub("_", " "), status))
+
+	end
+
+end
+
+
+
+
+
+
+
+
