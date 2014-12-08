bf4cd17e78070b71214328421f4efc2810011d8c
andy <andy@andydote.co.uk>
2014-12-08 22:58:32 +0000

    controls.dsl: add first recursive version

--- controls/dsl.lua
+++ controls/dsl.lua
@@ -4,10 +4,65 @@ local controls = ns.controls
 
 local controlDsl = {
 
-	process = function(self, input)
+	process = function(self, parent, input)
 
+		for i, config in ipairs(input) do
+
+			local controlType = input.type:lower()
+			local controlClass = controls[controlType]
+
+			if not controlClass then
+				error(string.format("Cannot find a control type called '%s'.", controlType))
+			end
+
+			input.parent = input.parent or parent
+
+			local instance = controlClass:new(input)
+
+			if input.controls then
+				self:process(instance, input.controls)
+			end
+
+		end
+
+		return parent
 
 	end,
 }
 
 ns.controls.dsl = controlDsl
+
+
+--[[
+local example = {
+	{
+		type = "label",
+		name = "$parentHeader",
+		points = { {} },
+		size = { 125, 20 }
+	},
+	{
+		type = "group",
+		name = "$parentGroup",
+		points = {
+			{ "TOPLEFT", "$parentHeader", "BOTTOMLEFT", 0, -5 },
+			{ "TOPRIGHT", "$parentHeader", "BOTTOMRIGHT", 0, -5 }
+		},
+		height = 200,
+		controls = {
+			{
+				type = "label",
+				name = "$parentLeft",
+				points = { {} },
+				size = { 125, 20 }
+			},
+			{
+				type = "label",
+				name = "$parentRight",
+				points = { {} },
+				size = { 125, 20 }
+			}
+		}
+	}
+}
+]]--
