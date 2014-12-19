3b71e0f6313d4ac4e4e36bdf33dec460a104142a
andy <andy@andydote.co.uk>
2014-12-19 23:11:08 +0000

    layout: finish common implementation

--- layout/common.lua
+++ layout/common.lua
@@ -12,6 +12,48 @@ local horizontal = {
 	shouldWrap = function(settings, container, child, x, y)
 		return x + child:GetWidth() > container:GetWidth()
 	end,
+
+	getXreset = function(settings, x, currentExpand)
+		return 0
+	end,
+
+	getYreset = function(settings, y, currentExpand)
+		return y + currentExpand + settings.itemSpacing
+	end,
+
+	getX = function(x, direction)
+		return x * direction
+	end,
+
+	getY = function(y, direction)
+		return -y
+	end,
+
+	getXnext = function(settings, x, child)
+		return x + child:GetWidth() + settings.itemSpacing
+	end,
+
+	getYnext = function(settings, y, child)
+		return y
+	end,
+
+	getExpand = function(currentExpand, child)
+
+		if child:GetHeight() > currentExpand then
+			currentExpand = child:GetHeight()
+		end
+
+		return currentExpand
+
+	end,
+
+	getContainerWidth = function(settings, currentExpand, x)
+		return x - settings.itemSpacing
+	end,
+
+	getContainerHeight = function(settings, currentExpand, y)
+		return y + currentExpand
+	end,
 }
 
 local vertical = {
@@ -28,6 +70,47 @@ local vertical = {
 		y + child:GetHeight() > container:GetHeight()
 	end,
 
+	getXreset = function(settings, x, currentExpand)
+		return x + currentExpand + settings.itemSpacing
+	end,
+
+	getYreset = function(settings, y, currentExpand)
+		return 0
+	end,
+
+	getX = function(x, direction)
+		return x
+	end,
+
+	getY = function(y, direction)
+		return y * direction
+	end,
+
+	getXnext = function(settings, x, child)
+		return x
+	end,
+
+	getYnext = function(settings, y, child)
+		return y + child:GetHeight() + settings.itemSpacing
+	end,
+
+	getExpand = function(currentExpand, child)
+
+		if child:GetWidth() > currentExpand then
+			currentExpand = child:GetWidth()
+		end
+
+		return currentExpand
+
+	end,
+
+	getContainerWidth = function(settings, currentExpand, x)
+		return x + currentExpand
+	end,
+
+	getContainerHeight = function(settings, currentExpand, y)
+		return y - settings.itemSpacing
+	end,
 }
 
 local engine = function(settings, actions, container, children)
@@ -35,17 +118,33 @@ local engine = function(settings, actions, container, children)
 	local direction = actions.getDirection()
 
 	local x, y = 0, 0
-
+	local currentExpand = 0
 
 	for i, child in ipairs(children) do
 
 		if settings.wrap and actions.shouldWrap(settings, container, child, x, y) then
-			-- ?
+			x = actions.getXreset(settings, x, currentExpand)
+			y = actions.getYreset(settings, y, currentExpand)
+			currentExpand = 0
 		end
 
 		child:ClearAllPoints()
-		child:SetPoint(settings.origin, container, settings.origin, )
+		child:SetPoint(settings.origin, container, settings.origin, actions.getX(x, direction), actions.getY(y, direction) )
+
+		x = actions.getXnext(settings, x, child)
+		y = actions.getYnext(settings, y, child)
+
+		currentExpand = actions.getExpand(currentExpand, child)
 
 	end
+
+	if settings.autosize == "x" or settings.autosize == "both" then
+		container:SetWidth(actions.getContainerWidth(settings, currentExpand, x))
+	end
+
+	if settings.autosize == "y" or settings.autosize == "both" then
+		container:SetHeight(actions.getContainerHeight(settings, currentExpand, y))
+	end
+
 
 end
