38c08671990faf8de6acd9c918835a2a54f78469
andy <andy@andydote.co.uk>
2014-12-22 17:35:13 +0000

    controls.dsl: split into single control and multiple control processing

--- controls/dsl.lua
+++ controls/dsl.lua
@@ -4,34 +4,43 @@ local controls = ns.controls
 
 local controlDsl = {
 
-	process = function(self, parent, input)
+	multiple = function(self, parent, input)
 
 		for i, config in ipairs(input) do
 
-			local controlType = config.type:lower()
-			local controlClass = controls[controlType]
-
-			if not controlClass then
-				error(string.format("Cannot find a control type called '%s'.", controlType))
-			end
-
-			local p = config.parent or parent
-
-			if p.frame then
-				p = p.frame
-			end
-
-			local instance = controlClass:new(p, config)
-
-			if config.controls then
-				self:process(instance, config.controls)
-			end
+			self:single(parent, config)
 
 		end
 
 		return parent
 
 	end,
+
+	single = function(self, parent, config)
+
+		local controlType = config.type:lower()
+		local controlClass = controls[controlType]
+
+		if not controlClass then
+			error(string.format("Cannot find a control type called '%s'.", controlType))
+		end
+
+		local p = config.parent or parent
+
+		if p.frame then
+			p = p.frame
+		end
+
+		local instance = controlClass:new(p, config)
+
+		if config.controls then
+			self:multiple(instance, config.controls)
+		end
+
+		return instance
+
+	end,
+
 }
 
 ns.controls.dsl = controlDsl
