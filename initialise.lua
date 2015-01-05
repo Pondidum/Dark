e9e3e9e63e18cb6de7ca993d21c8511ea8dab41a
andy <andy@andydote.co.uk>
2015-01-05 18:04:52 +0000

    media: import sounds from dark.core

--- initialise.lua
+++ initialise.lua
@@ -3,11 +3,6 @@ local addon, ns = ...
 ns.mixins = {}
 ns.layouts = {}
 ns.controls = {}
-
-local core = Dark.core
-
-ns.media = {
-	sounds = core.sounds,
-}
+ns.media = {}
 
 ns.layout = core.layout
---
+++ media/sounds.lua
@@ -0,0 +1,8 @@
+local addon, ns = ...
+
+local sounds = {
+	warning = [[Interface\AddOns\Dark\media\sounds\warning.mp3]],
+	whisper = [[Interface\AddOns\Dark\media\sounds\whisper.mp3]],
+}
+
+ns.media.sounds = sounds
