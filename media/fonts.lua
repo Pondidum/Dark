d348745243b1fc00b4f3ebf9b1971401742df2a0
andy <andy@andydote.co.uk>
2015-02-01 15:07:33 +0000

    fonts: add helper method to create standard looking font

--- media/fonts.lua
+++ media/fonts.lua
@@ -4,6 +4,23 @@ local fonts = {
 	normal 		= [[Interface\AddOns\Dark\media\fonts\PT-Sans-Narrow.ttf]],
 	unitframes	= [[Interface\AddOns\Dark\media\fonts\BigNoodleTitling.ttf]],
 	combat		= [[Interface\AddOns\Dark\media\fonts\Ultima_Campagnoli.ttf]],
+
+
+	create = function(self, parent, fontName, fontSize)
+
+	local font = fontName or self.normal
+	local size = fontSize or 12
+
+	local fs = parent:CreateFontString(nil, "OVERLAY")
+
+	fs:SetFont(font, size)
+	fs:SetJustifyH("LEFT")
+	fs:SetShadowColor(0, 0, 0)
+	fs:SetShadowOffset(1.25, -1.25)
+
+	return fs
+
+end
 }
 
 ns.media.fonts = fonts
