598a8d4cd006ba2b75ffdcb1c5338dfeacebeb71
andy <andy@andydote.co.uk>
2015-04-05 17:27:46 +0100

    controls.frame: allow template to be passed in too

--- controls/frame.lua
+++ controls/frame.lua
@@ -3,7 +3,12 @@ local controls = ns.controls
 
 controls.frame = function(self, config)
 
-	local frame = CreateFrame(config.type or self.defaults.type or "Frame", config.name, config.parent or self.defaults.parent)
+	local type = config.type or self.defaults.type or "Frame"
+	local name = config.name
+	local parent = config.parent or self.defaults.parent
+	local template = config.template
+
+	local frame = CreateFrame(type, name, parent, template)
 
 	frame:SetSize(config.width or self.defaults.width, config.height or self.defaults.height)
 
