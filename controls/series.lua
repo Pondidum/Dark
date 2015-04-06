0ec2871eb53a27630328ec66af5087b0b09b2131
andy <andy@andydote.co.uk>
2015-04-06 12:28:04 +0100

    controls.series: allow point and spacing to be specified

--- controls/series.lua
+++ controls/series.lua
@@ -1,16 +1,20 @@
 local addon, ns = ...
 local controls = ns.controls
 
-controls.series = function(self, ...)
+controls.series = function(self, config, ...)
 
 	local children = { ... }
+
+	local point = config.point or config.defaults.point or "BOTTOMLEFT"
+	local relPoint = config.relative or config.defaults.relative or "BOTTOMRIGHT"
+	local spacing = config.spacing or config.defaults.spacing or 0
 
 	for i = 2, #children do
 
 		local prev = children[i - 1]
 		local current = children[i]
 
-		current:SetPoint("BOTTOMLEFT", prev, "BOTTOMRIGHT", self.defaults.spacing, 0)
+		current:SetPoint(point, prev, relPoint, spacing, 0)
 
 	end
 
