3eb9df554003b91daef6d83445a985bbaa615eda
andy <andy@andydote.co.uk>
2015-04-12 18:37:46 +0100

    configReader: attempt to use :readConfigRow before by 1st parameter name

--- mixins/configReader.lua
+++ mixins/configReader.lua
@@ -4,18 +4,30 @@ local configReaderMixer = ns.mixin:extend({
 
 	readConfig = function(self, config)
 
+		local readRowMethodName = "readConfigRow"
+		local readRowMethod = self[readRowMethodName]
+
 		for i, row in ipairs(config) do
 
 			local methodName = row[1]
 			local method = self[methodName]
 
-			if not method then
-				error(string.format("Unable to find a method called '%s'", methodName))
+			if readRowMethod then
+				readRowMethod(self, unpack(row))
+
+			elseif method then
+				method(self, select(2, unpack(row)))
+
+			else
+				error(string.format(
+					"Unable to find a method called '%s' or '%s'",
+					methodName,
+					readRowMethodName))
+
 			end
 
-			method(self, select(2, unpack(row)))
-
 		end
+
 	end,
 })
 
