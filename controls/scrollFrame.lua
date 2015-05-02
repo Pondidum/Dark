42740c664b4d7ba5960270603801ac3440e80804
andy <andy@andydote.co.uk>
2015-05-02 15:08:40 +0100

    controls: scrollFrame now owns its only child frame, handles resizing

--- controls/scrollFrame.lua
+++ controls/scrollFrame.lua
@@ -7,30 +7,20 @@ controls.scrollFrame = function(self, config)
 	config.type = "ScrollFrame"
 	config.template = "UIPanelScrollFrameTemplate"
 
-	local frame = self:frame(config)
-	local bar = frame.ScrollBar
+	local scrollParent = self:frame(config)
+	local bar = scrollParent.ScrollBar
 
 	bar:ClearAllPoints()
-	bar:SetPoint("TOPRIGHT", frame, "TOPRIGHT", 0, -16)
-	bar:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", 0, 16)
+	bar:SetPoint("TOPRIGHT", scrollParent, "TOPRIGHT", 0, -16)
+	bar:SetPoint("BOTTOMRIGHT", scrollParent, "BOTTOMRIGHT", 0, 16)
 
-    frame.addChild = function(f, child)
+    local child = self:frame({ parent = scrollParent })
 
-        child:SetParent(frame)
-        frame:SetScrollChild(child)
+    scrollParent:SetScrollChild(child)
+    scrollParent:SetScript("OnSizeChanged", function(s, width, height)
+        child:SetWidth(width - bar:GetWidth())
+    end)
 
-        local onSizeChanged = function(frame, width, height)
-
-            local barWidth = bar:GetWidth()
-
-            child:SetWidth(width - barWidth)
-
-        end
-
-        frame:SetScript("OnSizeChanged", onSizeChanged)
-
-    end
-
-    return frame
+    return scrollParent
 
 end
