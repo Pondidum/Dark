c32fbbe5c0156d3f5eb159dad26103bbd069d39b
andy <andy@andydote.co.uk>
2015-05-04 15:36:45 +0100

    controls.listbox: use own layout engine which auto locates child widths

--- controls/listbox.lua
+++ controls/listbox.lua
@@ -2,13 +2,25 @@ local addon, ns = ...
 local controls = ns.controls
 local layout = ns.layoutEngine
 
+local listEngine = layout:extend({
+
+	afterLayout = function(self)
+
+		for i, child in ipairs(self.children) do
+			child:SetPoint("LEFT", self.container, "LEFT", 2, 0)
+			child:SetPoint("RIGHT", self.container, "RIGHT", -2, 0)
+		end
+
+	end,
+})
+
 controls.listbox = function(self, config)
 
 	config = config or {}
 
 	local scrollBox = self:scrollFrame({ name = config.name, width = config.width })
 	local contents = scrollBox:GetScrollChild()
-	local engine = layout:new(contents, {
+	local engine = listEngine:new(contents, {
 		layout = "vertical",
 		origin = "TOP",
 		wrap = false,
